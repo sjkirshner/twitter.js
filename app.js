8fe6a678f8c737a23436effeb08e114c8f18e1bb
Sara Jo Helba <sarahelba@Saras-MacBook-Pro.local>
2017-08-01 12:59:03 -0400

    sara 2

--- app.js
+++ app.js
@@ -2,15 +2,8 @@ const express = require('express');
 const app = express();
 const chalk = require('chalk');
 const nunjucks = require('nunjucks');
+const routes = require('./routes');
 
-let locals = {
-    title: 'An Example',
-    people: [
-        { name: 'Gandalf'},
-        { name: 'Frodo' },
-        { name: 'Hermione'}
-    ]
-};
 
 
 nunjucks.configure('views', {noCache: true});
@@ -22,22 +15,20 @@ nunjucks.render('index.html', locals, function (err, output) {
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 
+// app.use(function(req,res,next){ //MIDDLEWARE
+//   console.log(chalk.yellow("Your verb is " + req.method + ". And your url is " + req.url))
+//   next();
+// })
 
-app.use(function(req,res,next){
-  console.log(chalk.yellow("Your verb is " + req.method + ". And your url is " + req.url))
-  next();
-})
+app.use('/', routes);
 
-app.get('/', function(req,res){
-  const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-  res.render( 'index', {title: 'Hall of Fame', people: people} );
 
-})
-app.get('/news',function(req,res){
-  const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-  res.render( 'index', {title: 'Hall of Fame', people: people} );
 
-})
+
+
+
+
+
 
 
 app.listen(3000, function(){
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,22 @@
+const express = require('express');
+const router = express.Router();
+// could use one line instead: const router = require('express').Router();
+const tweetBank = require('../tweetBank');
+
+router.get('/', function (req, res) {
+  let tweets = tweetBank.list();
+  res.render( 'index', { tweets: tweets } );
+});
+
+router.get('/stylesheets/style.css', function(req,res){
+  res.sendFile('../public/stylesheets/style.css');
+})
+
+module.exports = router;
+
+
+
+
 // const tweetBank = require('../tweetBank');
 
 // tweetBank.add('gabe', 'mindblown');
